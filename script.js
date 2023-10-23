46b9c525d08f09b45308d42d05790dfc2bd2ff04
Hagliberto <hagliberto@gmail.com>
2023-10-22 22:15:53 -0300

    Função para contar caracteres em branco

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var textarea = document.getElementById('text-input');
-  var wordCountSpan = document.getElementById('word-count');
-  var charCountSpan = document.getElementById('char-count');
-  var countWordsBtn = document.getElementById('count-words-btn');
-  var countCharsBtn = document.getElementById('count-chars-btn');
-  var clearBtn = document.getElementById('clear-btn');
-
-  function countWords() {
-    var text = textarea.value;
-    var words = text.trim().split(/\s+/);
-    var wordCount = words.length;
-
-    wordCountSpan.textContent = wordCount;
-  }
-
-  function countChars() {
-    var text = textarea.value;
-    var charCount = text.length;
-
-    charCountSpan.textContent = charCount;
-  }
-
-  countWordsBtn.addEventListener('click', function() {
-    countWords();
+document.addEventListener('DOMContentLoaded', function() {
+    var textInput = document.getElementById('text-input');
+    var wordCount = document.getElementById('word-count');
+    var charCount = document.getElementById('char-count');
+    var whitespaceCount = document.getElementById('whitespace-count');
+    var countWordsBtn = document.getElementById('count-words-btn');
+    var countCharsBtn = document.getElementById('count-chars-btn');
+    var clearBtn = document.getElementById('clear-btn');
+  
+    countWordsBtn.addEventListener('click', function() {
+      var text = textInput.value.trim();
+      var words = text.split(/\s+/).filter(function(word) {
+        return word.length > 0;
+      });
+      wordCount.textContent = words.length;
+    });
+  
+    countCharsBtn.addEventListener('click', function() {
+      var text = textInput.value;
+      var nonSpaceChars = text.replace(/\s/g, '');
+      var whitespaceChars = text.length - nonSpaceChars.length;
+      charCount.textContent = nonSpaceChars.length;
+      whitespaceCount.textContent = whitespaceChars;
+    });
+  
+    clearBtn.addEventListener('click', function() {
+      textInput.value = '';
+      wordCount.textContent = '0';
+      charCount.textContent = '0';
+      whitespaceCount.textContent = '0';
+    });
   });
-
-  countCharsBtn.addEventListener('click', function() {
-    var text = textarea.value;
-    var charCountWithSpaces = text.length;
-    var charCountWithoutSpaces = text.replace(/\s+/g, '').length;
-
-    charCountSpan.textContent = charCountWithSpaces + " (incluindo espaços em branco: " + charCountWithoutSpaces + ")";
-  });
-
-  clearBtn.addEventListener('click', function() {
-    textarea.value = '';
-    wordCountSpan.textContent = '0';
-    charCountSpan.textContent = '0';
-  });
-});
