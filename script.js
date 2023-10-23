7380844d4945b6db47b6c4dee1cb1e596bb0ccc2
Hagliberto <hagliberto@gmail.com>
2023-10-22 23:07:22 -0300

    Adição de um gráfico relacionando as contagens

--- script.js
+++ script.js
@@ -6,21 +6,55 @@ document.addEventListener('DOMContentLoaded', function() {
     var countWordsBtn = document.getElementById('count-words-btn');
     var countCharsBtn = document.getElementById('count-chars-btn');
     var clearBtn = document.getElementById('clear-btn');
+    var chartCtx = document.getElementById('chart').getContext('2d');
+  
+    var chartData = {
+        labels: ['Palavras', 'Caracteres', 'Caracteres em Branco'],
+        datasets: [{
+          label: 'Contagem',
+          data: [0, 0, 0],
+          backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+          borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+          borderWidth: 1
+        }]
+      };
+  
+    var chartConfig = {
+      type: 'bar',
+      data: chartData,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
+
+  
+    var chart = new Chart(chartCtx, chartConfig);
   
     countWordsBtn.addEventListener('click', function() {
       var text = textInput.value.trim();
       var words = text.split(/\s+/).filter(function(word) {
         return word.length > 0;
       });
-      wordCount.textContent = words.length;
+      var wordCountValue = words.length;
+      wordCount.textContent = wordCountValue;
+      chartData.datasets[0].data[0] = wordCountValue;
+      chart.update();
     });
   
     countCharsBtn.addEventListener('click', function() {
       var text = textInput.value;
       var nonSpaceChars = text.replace(/\s/g, '');
       var whitespaceChars = text.length - nonSpaceChars.length;
-      charCount.textContent = nonSpaceChars.length;
+      var charCountValue = nonSpaceChars.length;
+      charCount.textContent = charCountValue;
       whitespaceCount.textContent = whitespaceChars;
+      chartData.datasets[0].data[1] = charCountValue;
+      chartData.datasets[0].data[2] = whitespaceChars;
+      chart.update();
     });
   
     clearBtn.addEventListener('click', function() {
@@ -28,5 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
       wordCount.textContent = '0';
       charCount.textContent = '0';
       whitespaceCount.textContent = '0';
+      chartData.datasets[0].data = [0, 0, 0];
+      chart.update();
     });
   });
