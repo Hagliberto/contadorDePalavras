9961b26fb576ccaee82a8bc06eca3017eb119ec8
Hagliberto <hagliberto@gmail.com>
2023-10-22 21:53:24 -0300

    Adição de funções de contagens e de reset

--- script.js
+++ script.js
@@ -2,14 +2,36 @@ window.addEventListener('DOMContentLoaded', function() {
     var textarea = document.getElementById('text-input');
     var wordCountSpan = document.getElementById('word-count');
     var charCountSpan = document.getElementById('char-count');
-    
-    textarea.addEventListener('input', function() {
+    var countWordsBtn = document.getElementById('count-words-btn');
+    var countCharsBtn = document.getElementById('count-chars-btn');
+    var clearBtn = document.getElementById('clear-btn');
+  
+    function countWords() {
       var text = textarea.value;
       var words = text.trim().split(/\s+/);
       var wordCount = words.length;
-      var charCount = text.length;
-      
+  
       wordCountSpan.textContent = wordCount;
+    }
+  
+    function countChars() {
+      var text = textarea.value;
+      var charCount = text.length;
+  
       charCountSpan.textContent = charCount;
+    }
+  
+    countWordsBtn.addEventListener('click', function() {
+      countWords();
+    });
+  
+    countCharsBtn.addEventListener('click', function() {
+      countChars();
+    });
+  
+    clearBtn.addEventListener('click', function() {
+      textarea.value = '';
+      wordCountSpan.textContent = '0';
+      charCountSpan.textContent = '0';
     });
   });
